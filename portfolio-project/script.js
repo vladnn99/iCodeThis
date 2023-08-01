7603dc7fffd34e954db11d16f88c46f3b998b745
vladnn99 <vladnegrean@gmail.com>
2023-08-01 19:47:06 +0300

    updates

--- portfolio-project/script.js
+++ portfolio-project/script.js
@@ -1,17 +1,72 @@
 console.log(`Hello world`);
 const nav = document.querySelector("nav");
-console.log(nav);
+const avatar = document.getElementById("avatar");
+const slider = document.getElementById("slider");
+const menu = document.getElementById("menu");
+const menuLinks = [...document.getElementsByClassName("links")];
+console.log(avatar);
+console.log(slider);
+console.log(menu);
+
+function addIds(menuLinks) {
+  menuLinks.forEach((link, index) => {
+    link.id = index;
+  });
+}
+
+addIds(menuLinks);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    const clickedLinkId = +event.target.id;
+    if (clickedLinkId === 1) {
+      slider.classList.add("left-1/3");
+      slider.classList.remove("left-0", "left-2/3");
+      menuLinks[clickedLinkId].classList.add("text-white");
+      menuLinks[clickedLinkId - 1].classList.remove("text-white");
+      menuLinks[clickedLinkId + 1].classList.remove("text-white");
+    }
+
+    if (clickedLinkId === 2) {
+      slider.classList.add("left-2/3");
+      slider.classList.remove("left-0", "left-1/3");
+      menuLinks[clickedLinkId].classList.add("text-white");
+      menuLinks[clickedLinkId - 1].classList.remove("text-white");
+      menuLinks[clickedLinkId - 2].classList.remove("text-white");
+    }
+
+    if (clickedLinkId === 0) {
+      slider.classList.add("left-0");
+      slider.classList.remove("left-1/3", "left-2/3");
+      menuLinks[clickedLinkId].classList.add("text-white");
+      menuLinks[clickedLinkId + 1].classList.remove("text-white");
+      menuLinks[clickedLinkId + 2].classList.remove("text-white");
+    }
+  });
+});
+
+console.log(menuLinks);
+
+// console.log(nav.childNodes[3].childNodes[1].childNodes[1].childNodes);
+let moveMenu = false;
 
 const handleScroll = () => {
   const scrollY = window.scrollY;
-
-  // Add or remove the "scrolled" class based on the scroll position
-  if (scrollY > 25) {
-    nav.classList.add("fixed", "top-0", "opacity-0");
-    setTimeout(function () {
-      nav.classList.add("opacity-100");
-      nav.classList.remove("opacity-0");
-    }, 500);
+  if (scrollY > 10 && !moveMenu) {
+    moveMenu = true;
+    avatar.classList.remove("w-32", "h-32");
+    avatar.classList.add("w-16", "h-16");
+    nav.classList.remove("h-40");
+    nav.classList.add("h-[5.5rem]", "shadow-lg");
+  }
+  if (scrollY < 10 && moveMenu) {
+    moveMenu = false;
+    // nav.classList.add("opacity-0");
+    avatar.classList.remove("w-16", "h-16");
+    avatar.classList.add("w-32", "h-32");
+    nav.classList.remove("h-[5.5rem]");
+    nav.classList.remove("shadow-lg");
+    nav.classList.add("h-40");
   }
 };
 
