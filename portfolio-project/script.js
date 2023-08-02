06a4297afe565fa64ded73b9217ce832fea34b60
vladnn99 <vladnegrean@gmail.com>
2023-08-02 11:27:16 +0300

    scroll targets updated with window.scrollTo

--- portfolio-project/script.js
+++ portfolio-project/script.js
@@ -4,6 +4,8 @@ const avatar = document.getElementById("avatar");
 const slider = document.getElementById("slider");
 const menu = document.getElementById("menu");
 const menuLinks = [...document.getElementsByClassName("links")];
+const targetSections = [];
+
 console.log(avatar);
 console.log(slider);
 console.log(menu);
@@ -14,7 +16,33 @@ function addIds(menuLinks) {
   });
 }
 
+function getTargetSections() {
+  menuLinks.forEach((link) => {
+    const targetId =
+      link.getAttribute("href") !== "#" ? link.getAttribute("href") : null;
+    const targetSection = document.querySelector(targetId);
+    targetSections.push({ targetSection: targetSection, targetOffset: "" });
+  });
+  targetSections.shift();
+  targetSections[0].targetOffset =
+    targetSections[0].targetSection.offsetTop - 76;
+  targetSections[1].targetOffset =
+    targetSections[1].targetSection.offsetTop + 296;
+
+  console.log(targetSections);
+}
+
+getTargetSections();
 addIds(menuLinks);
+
+function scrollToSection(event, clickedLinkId) {
+  event.preventDefault();
+  window.scrollTo({
+    top: targetSections[clickedLinkId - 1].targetOffset,
+    behavior: "smooth",
+  });
+  console.log(targetSections[clickedLinkId - 1]);
+}
 
 menuLinks.forEach((link) => {
   link.addEventListener("click", function (event) {
@@ -25,6 +53,7 @@ menuLinks.forEach((link) => {
       menuLinks[clickedLinkId].classList.add("text-white");
       menuLinks[clickedLinkId - 1].classList.remove("text-white");
       menuLinks[clickedLinkId + 1].classList.remove("text-white");
+      scrollToSection(event, clickedLinkId);
     }
 
     if (clickedLinkId === 2) {
@@ -33,6 +62,7 @@ menuLinks.forEach((link) => {
       menuLinks[clickedLinkId].classList.add("text-white");
       menuLinks[clickedLinkId - 1].classList.remove("text-white");
       menuLinks[clickedLinkId - 2].classList.remove("text-white");
+      scrollToSection(event, clickedLinkId);
     }
 
     if (clickedLinkId === 0) {
@@ -50,16 +80,16 @@ console.log(menuLinks);
 // console.log(nav.childNodes[3].childNodes[1].childNodes[1].childNodes);
 let moveMenu = false;
 
-const handleScroll = () => {
+function handleScroll() {
   const scrollY = window.scrollY;
-  if (scrollY > 10 && !moveMenu) {
+  if (scrollY > 0 && !moveMenu) {
     moveMenu = true;
     avatar.classList.remove("w-32", "h-32");
     avatar.classList.add("w-16", "h-16");
     nav.classList.remove("h-40");
     nav.classList.add("h-[5.5rem]", "shadow-lg");
   }
-  if (scrollY < 10 && moveMenu) {
+  if (scrollY === 0 && moveMenu) {
     moveMenu = false;
     // nav.classList.add("opacity-0");
     avatar.classList.remove("w-16", "h-16");
@@ -68,6 +98,6 @@ const handleScroll = () => {
     nav.classList.remove("shadow-lg");
     nav.classList.add("h-40");
   }
-};
+}
 
 window.addEventListener("scroll", handleScroll);
