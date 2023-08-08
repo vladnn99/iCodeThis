9eb95c6660067f4f02bcb766642c9cfaf09ee986
vladnn99 <vladnegrean@gmail.com>
2023-08-08 19:05:19 +0300

    category filters

--- portfolio-project/script.js
+++ portfolio-project/script.js
@@ -61,13 +61,58 @@ const projects = [
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia aliquid quas itaque, adipisci neque nihil tempore, nulla dolorem ad fugit officiis saepe reiciendis non nesciunt, dolor eligendi a sapiente dignissimos impedit doloremque optio nostrum omnis rem nam! Iure molestias animi excepturi temporibus alias incidunt distinctio provident praesentium dolorum dolore. Cumque.",
   },
 ];
+const projectsContainer = document.getElementById("projectsContainer");
+const projectTypes = ["Design + Development", "Design", "Development"];
+let clickedIndex = 0;
 
-console.log(avatar);
-console.log(slider);
-console.log(menu);
-console.log(menuLinks);
-console.log(filterButtons);
-console.log(projects);
+function assignProjectId() {
+  projects.forEach((project, index) => (project.id = index));
+}
+
+assignProjectId();
+
+// console.log(avatar);
+// console.log(slider);
+// console.log(menu);
+// console.log(menuLinks);
+// console.log(filterButtons);
+// console.log(projects);
+// console.log(projectsContainer);
+
+// Generate project view
+function renderProjects(clickedIndex) {
+  const filteredProjects = projects.filter((project) => {
+    if (clickedIndex === 0) {
+      return (
+        project.Type === projectTypes[0] ||
+        project.Type === projectTypes[1] ||
+        project.Type === projectTypes[2]
+      );
+    }
+    if (clickedIndex === 1 || clickedIndex === 2) {
+      return project.Type === projectTypes[clickedIndex];
+    }
+  });
+  // console.log(filteredProjects);
+  filteredProjects.forEach((project) => {
+    const projectDiv = document.createElement("div");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    img.className = "object-contain w-full";
+    img.src = `${project.coverImage}`;
+    h2.innerText = `${project.Name}`;
+    p.className = "text-xs";
+    p.innerHTML = `${project.Type} <span class="ml-2">2023</span>`;
+    projectDiv.className = "flex flex-col gap-2 mb-8";
+    projectDiv.appendChild(img);
+    projectDiv.appendChild(h2);
+    projectDiv.appendChild(p);
+    projectsContainer.appendChild(projectDiv);
+  });
+}
+
+renderProjects(clickedIndex);
 
 function addIds(menuLinks) {
   menuLinks.forEach((link, index) => {
@@ -77,21 +122,21 @@ function addIds(menuLinks) {
 
 function getTargetSections() {
   menuLinks.forEach((link) => {
-    console.log(link);
+    // console.log(link);
     const targetId =
       link.getAttribute("href") !== "#" ? link.getAttribute("href") : "null";
     const targetSection = document.querySelector(targetId);
     targetSections.push({ targetSection: targetSection, targetOffset: "" });
   });
   const navTargetId = document.getElementById("navTop");
-  console.log(navTargetId);
+  // console.log(navTargetId);
   targetSections[0].targetOffset = 0;
   targetSections[0].targetSection = navTargetId;
   // targetSections.shift();
   targetSections[1].targetOffset =
     targetSections[1].targetSection.offsetTop - 88;
   targetSections[2].targetOffset = targetSections[2].targetSection.offsetTop;
-  console.log(targetSections);
+  // console.log(targetSections);
 }
 
 getTargetSections();
@@ -105,6 +150,7 @@ function scrollToSection(event, clickedLinkId) {
   });
 }
 
+// Sliding focus on the menu
 menuLinks.forEach((link) => {
   link.addEventListener("click", function (event) {
     const clickedLinkId = +event.target.id;
@@ -138,7 +184,7 @@ menuLinks.forEach((link) => {
   });
 });
 
-console.log(menuLinks);
+// console.log(menuLinks);
 
 let moveMenu = false;
 
@@ -163,24 +209,30 @@ function handleScroll() {
 
 window.addEventListener("scroll", handleScroll);
 
-filterButtons.forEach((button, index) => {
-  button.addEventListener("click", function () {
-    if (index === 0) {
-      button.classList.add("bg-[#1F1E26]", "text-white");
-      filterButtons[1].classList.remove("text-white", "bg-[#1F1E26]");
-      filterButtons[2].classList.remove("text-white", "bg-[#1F1E26]");
-    }
-    if (index === 1) {
-      button.classList.add("bg-[#1F1E26]", "text-white");
-      filterButtons[0].classList.remove("text-white", "bg-[#1F1E26]");
-      filterButtons[2].classList.remove("text-white", "bg-[#1F1E26]");
-    }
-    if (index === 2) {
-      button.classList.add("bg-[#1F1E26]", "text-white");
-      filterButtons[0].classList.remove("text-white", "bg-[#1F1E26]");
-      filterButtons[1].classList.remove("text-white", "bg-[#1F1E26]");
-    }
+function setFilterButtons() {
+  filterButtons.forEach((button, index) => {
+    button.addEventListener("click", function (event) {
+      filterButtons.forEach((btn, idx) => {
+        if (idx === index) {
+          btn.classList.add("bg-[#1F1E26]", "text-white");
+        } else {
+          btn.classList.remove("bg-[#1F1E26]", "text-white");
+        }
+      });
+      clickedIndex = index;
+      projectsContainer.classList.add("opacity-0");
+      setTimeout(function () {
+        projectsContainer.innerHTML = "";
+        renderProjects(clickedIndex);
+        projectsContainer.classList.remove("opacity-0");
+      }, 300);
+      setTimeout(function () {
+        console.log(projectsContainer.className);
+      }, 300);
+    });
   });
-});
+}
 
-// Incearca 2 coloane cu grid si inauntru flex pt afisare portfolio in foma din figma
+setFilterButtons();
+
+// Mai trebuie sa fac animatiile din tailwind pt filtrarea proiectelor si sa vad de ce nu se extinde containerul mai jos
