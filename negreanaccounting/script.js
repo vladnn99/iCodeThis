071b2b2a9ba03fa768f68ccbe0bf796815bd4037
vladnn99 <vladnegrean@gmail.com>
2023-09-06 21:12:43 +0300

    slider updates

--- negreanaccounting/script.js
+++ negreanaccounting/script.js
@@ -5,14 +5,20 @@ const sliderImages = [
   {
     img: "files/img_1",
     alt: "NEGREANaccounting.com slider image 1 contabilitate",
+    text: `Ana1 are mere
+    Ana1 are si pere`,
   },
   {
     img: "files/img_2",
     alt: "NEGREANaccounting.com slider image 2 salarizare",
+    text: `Ana2 are mere
+    Ana2 are si pere`,
   },
   {
     img: "files/img_3",
     alt: "NEGREANaccounting.com slider image 3 contracte de muncă",
+    text: `Ana3 are mere
+    Ana3 are si pere`,
   },
 ];
 let moveMenu = false;
@@ -50,3 +56,49 @@ function handleViewportChange() {
 }
 
 window.addEventListener("resize", handleViewportChange);
+
+const sliderElements = [...document.querySelectorAll(".slider-element")];
+console.log(sliderElements);
+
+function imageSlider() {
+  setTimeout(function () {
+    sliderElements[0].classList.add("-translate-x-full");
+    sliderElements[0].classList.remove("-translate-x-full");
+    sliderElements[0].classList.add("translate-x-full");
+    console.log(sliderElements[0].className);
+    sliderElements[1].classList.remove("translate-x-full");
+  }, 3000);
+  setTimeout(function () {
+    sliderElements[0].classList.add("hidden");
+    console.log(sliderElements[0].className);
+    sliderElements[1].classList.add("-translate-x-full");
+    sliderElements[2].classList.remove("translate-x-full");
+  }, 6000);
+  setTimeout(function () {
+    sliderElements[0].classList.remove("hidden");
+    sliderElements[2].classList.add("-translate-x-full");
+    sliderElements[0].classList.remove("translate-x-full");
+    console.log(sliderElements[0].className);
+  }, 9000);
+}
+
+imageSlider();
+
+// let currentIndex = 0;
+
+// function showImage(index) {
+//   sliderElements.forEach((element, i) => {
+//     if (i === index) {
+//       element.classList.remove("translate-x-full");
+//     } else {
+//       element.classList.add("translate-x-full");
+//     }
+//   });
+// }
+
+// function nextImage() {
+//   currentIndex = (currentIndex + 1) % sliderElements.length;
+//   showImage(currentIndex);
+// }
+
+// setInterval(nextImage, 3000);
