417cf0621a844a99ce2c69483d11b1a0ee0e5e8a
vladnn99 <vladnegrean@gmail.com>
2023-09-07 17:40:18 +0300

    finished slider

--- negreanaccounting/script.js
+++ negreanaccounting/script.js
@@ -61,44 +61,15 @@ const sliderElements = [...document.querySelectorAll(".slider-element")];
 console.log(sliderElements);
 
 function imageSlider() {
+  sliderElements[0].classList.add("-translate-x-full");
+  sliderElements[1].classList.remove("translate-x-full");
+  sliderElements.push(sliderElements[0]);
+  sliderElements.shift();
   setTimeout(function () {
-    sliderElements[0].classList.add("-translate-x-full");
-    sliderElements[0].classList.remove("-translate-x-full");
-    sliderElements[0].classList.add("translate-x-full");
-    console.log(sliderElements[0].className);
-    sliderElements[1].classList.remove("translate-x-full");
-  }, 3000);
-  setTimeout(function () {
-    sliderElements[0].classList.add("hidden");
-    console.log(sliderElements[0].className);
-    sliderElements[1].classList.add("-translate-x-full");
-    sliderElements[2].classList.remove("translate-x-full");
-  }, 6000);
-  setTimeout(function () {
-    sliderElements[0].classList.remove("hidden");
-    sliderElements[2].classList.add("-translate-x-full");
-    sliderElements[0].classList.remove("translate-x-full");
-    console.log(sliderElements[0].className);
-  }, 9000);
+    sliderElements[2].className =
+      "w-full h-full flex slider-element absolute translate-x-full transition-transform duration-1000 ease-in-out hidden";
+  }, 1000);
+  sliderElements[1].classList.remove("hidden");
 }
 
-imageSlider();
-
-// let currentIndex = 0;
-
-// function showImage(index) {
-//   sliderElements.forEach((element, i) => {
-//     if (i === index) {
-//       element.classList.remove("translate-x-full");
-//     } else {
-//       element.classList.add("translate-x-full");
-//     }
-//   });
-// }
-
-// function nextImage() {
-//   currentIndex = (currentIndex + 1) % sliderElements.length;
-//   showImage(currentIndex);
-// }
-
-// setInterval(nextImage, 3000);
+setInterval(imageSlider, 7000);
